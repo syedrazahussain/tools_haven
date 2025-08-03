2f3dfffe2be24483245dbcdd5f9d928b2814ce0b
syedrazahussain <srazahussain1514@gmail.com>
2025-08-03 13:57:56 +0530

    updated ui testing

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,9 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+
+
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -19,6 +22,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
