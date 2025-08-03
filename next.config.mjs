4fab873f60a66302572acc66adb9ba25135a6fb5
syedrazahussain <srazahussain1514@gmail.com>
2025-08-03 16:50:47 +0530

    updated next

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-<<<<<<< HEAD
-    domains: ['tools-haven-server.onrender.com'],// 👈 Allow image loading from localhost
-=======
-     domains: ['localhost'],      // 👈 Allow image loading from localhost
+     //domains: ['localhost'],      // 👈 Allow image loading from localhost
 
-    // domains: ['tools-haven-server.onrender.com'],
->>>>>>> 48d0156 (updated content)
+     domains: ['tools-haven-server.onrender.com'],
   },
 };
 
